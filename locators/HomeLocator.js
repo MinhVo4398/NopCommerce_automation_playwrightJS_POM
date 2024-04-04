c0d0ad56e2ea5c3278da64795b528c31ddc1f936
shihab0005 <shihab0000000000@gmail.com>
2024-04-05 04:23:10 +0600

    auto

--- locators/HomeLocator.js
+++ locators/HomeLocator.js
@@ -4,5 +4,5 @@ export const productPrice = "(//span[@class='price actual-price'])[1]";
 export const searchField = "#small-searchterms";
 export const searchBtn = "button.button-1.search-box-button";
 export const productCart = "(//div[@class='item-box'])[1]";
-export const addToCartBtn = "#add-to-cart-button-4";
+export const addToCartBtn = "(//button[text()='Add to cart'])[1]";
 export const successNotificationBar = ".bar-notification.success";
